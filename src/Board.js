f7129bb57271803e28fd25699deec7924575b2f0
dafabulousteach <dafabulousteach@gmail.com>
2015-07-07 10:55:22 -0700

    Refactored all helper functions Helper functions passed

--- src/Board.js
+++ src/Board.js
@@ -138,12 +138,30 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var pieceCount = 0;
+      var major = majorDiagonalColumnIndexAtFirstRow;
+      var rows = this.rows();
+
+      for(var i = 0; i < rows.length; i++){
+        if(rows[i][major] === 1){
+          pieceCount++;
+          major++;
+        }
+      }
+      return !!(pieceCount > 1);
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+      var hasConflict = false;
+      var rows = this.rows();
+
+      for(var i = 0; i < rows.length; i++){
+        if(this.hasMajorDiagonalConflictAt(i)){
+          hasConflict = true;
+        }
+      }
+     return hasConflict;
     },
 
 
@@ -153,12 +171,34 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var pieceCount = 0;
+      var minor = minorDiagonalColumnIndexAtFirstRow;
+      var rows = this.rows();
+
+      for(var i = 0; i < rows.length; i++) {
+        if(rows[i][minor] === 1) {
+          pieceCount++;
+          minor--;
+        }
+      }
+
+
+      return !!(pieceCount > 1); // fixme
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      var hasConflict = false;
+      var rows = this.rows();
+
+      for(var i = 0; i < rows.length; i++) {
+        if(this.hasMinorDiagonalConflictAt(i)) {
+          hasConflict = true;
+        }
+      }
+
+
+      return hasConflict; // fixme
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
